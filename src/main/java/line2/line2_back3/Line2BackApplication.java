1769df20088639ef2cf76d23a3390ec8dbbe52ab
HD <gudehdu10@gmail.com>
2022-04-18 02:10:50 +0000

    test

--- src/main/java/line2/line2_back3/Line2BackApplication.java
+++ src/main/java/line2/line2_back3/Line2BackApplication.java
@@ -2,7 +2,9 @@ package line2.line2_back3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +14,8 @@ public class Line2BackApplication {
         SpringApplication.run(Line2BackApplication.class, args);
     }
 	
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/main/java/line2/line2_back3/restApi/RestApiService.java
+++ src/main/java/line2/line2_back3/restApi/RestApiService.java
@@ -3,6 +3,7 @@ package line2.line2_back3.restApi;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -20,13 +21,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class RestApiService {
-    String reservationServer = "http://openjdk:8080";
-    String userServer = "http://openjdk2:8080";
-    // String reservationServer = "http://localhost:8080";
-    // String userServer = "http://localhost:8081";
+    @Value("${user-server-url}")
+    String userServer;
+
+    @Value("${reservation-server-url}")
+    String reservationServer;
+
+    private final RestTemplate restTemplate;
 
     public User getUserById(Long id) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = userServer + "/user/v1/user/" + Long.toString(id);
         ResponseEntity<User> response = restTemplate.getForEntity(url, User.class);
         log.info("{}", response);
@@ -34,7 +37,6 @@ public class RestApiService {
     }
 
     public void deleteAllReservation(List<Reservation> reservations) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = reservationServer + "/book/v1/reservation/";
         reservations.forEach(reservation -> {
             restTemplate.delete(url + Long.toString(reservation.getId()));
@@ -42,7 +44,6 @@ public class RestApiService {
     }
 
     public List<Reservation> getByRoomId(Long id) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = reservationServer + "/book/v1/reservation/room/" + Long.toString(id);
         ResponseEntity<List<Reservation>> response = restTemplate.exchange(url, HttpMethod.GET, null,
                 new ParameterizedTypeReference<List<Reservation>>() {
@@ -52,7 +53,6 @@ public class RestApiService {
     }
 
     public List<Reservation> getByHomeId(Long id) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = reservationServer + "/book/v1/reservation/home/" + Long.toString(id);
         ResponseEntity<List<Reservation>> response = restTemplate.exchange(url, HttpMethod.GET, null,
                 new ParameterizedTypeReference<List<Reservation>>() {
@@ -62,7 +62,6 @@ public class RestApiService {
     }
 
     public int getHeadCount(ReservationHeadCountDto reservationHeadCountDto) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = reservationServer + "/book/v1/reservation/head_count";
         int headCount = restTemplate.postForObject(url, reservationHeadCountDto, int.class);
         log.info("{}", headCount);
